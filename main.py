94d941d19d418c7351d76d217fa5488a7b25caca
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2023-05-23 15:34:04 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# N (população de alunos na escola)
+# n (Tamanho da amostra, quantidade de pessoas entrevistadas)
+# n0 (Tamanho inicial da amostra com erro incluso)
+# γ (Grau de confiança)
+# α (Probabilidade de erro esperado)
+
+N = 480 
+# Total de alunos da escola
+
+n = int(input("Quantidade de pessoas entrevistadas: "))
+
+n0 = 
