39fca1d15dc523727a5abf8cbc5756bebbf4e6f2
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2023-05-24 17:10:16 -0300

    main.py

--- main.py
+++ main.py
@@ -4,9 +4,23 @@
 # γ (Grau de confiança)
 # α (Probabilidade de erro esperado)
 
-N = 480 
+N = int(input("População da escola: "))
 # Total de alunos da escola
 
 n = int(input("Quantidade de pessoas entrevistadas: "))
 
-n0 = 
+n0 = (N * n) / (N - n)
+#formula inicial --> n = (N * n0) / (N + n0)
+
+print(f"n0 = {n0}")
+
+α	 = 1/ n0
+#formula inicial --> n0 = 1 / α
+
+print(f"α = {α}")
+
+γ	= 1 - α
+#formula inicial α = 1 - γ
+
+print(f"Grau de confiança é {γ}")
+
