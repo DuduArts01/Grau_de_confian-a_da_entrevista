d21af0df5170cff306b97ea61ce61df69853e336
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2023-05-26 15:20:56 -0300

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 
 # N (população de alunos na escola)
 # n (Tamanho da amostra, quantidade de pessoas entrevistadas)
@@ -13,16 +14,59 @@ n = int(input("Quantidade de pessoas entrevistadas: "))
 n0 = (N * n) / (N - n)
 #formula inicial --> n = (N * n0) / (N + n0)
 
-α	 = 1/ n0
+print(f"\nn0 = {n0}")
+
+α	 = math.sqrt(1 / n0)
 #formula inicial --> n0 = 1 / α
+
+print(f"\nα = {α}")
 
 γ	= 1 - α
 #formula inicial α = 1 - γ
+
+print(f"\nγ = {γ}")
 
 print("-------------------------------------")
 print(f"\nGrau de confiança é {(γ * 100):.2f}%\n")
 print("-------------------------------------")
 #multiplica o γ por 100 para dar em porcentagem 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
